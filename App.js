e52cdca9926d331ee3afbb4c32dfdd010a05162d
Joao Almancio <joao.almancio@globalservs.com>
2021-02-20 17:04:24 -0300

    Passando projeto para o git.

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { TextInput, Text, View, Button, ScrollView, FlatList } from 'react-native';
+
+import styles from "./App.styles"
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+  const addGoalHandler = () => {
+    setCourseGoals((current) => {
+      return [...current, enteredGoal]
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          onChangeText={goalInputHandler}
+          style={styles.courseGoalInput}
+          value={enteredGoal} />
+        <Button
+          title="Add"
+          onPress={addGoalHandler} />
+      </View>
+
+      <FlatList
+        data={courseGoals}
+        keyExtractor={(item, index) => item+index}
+        renderItem={(itemData) => {
+          return (
+            <View style={styles.listItem}>
+              <Text>{itemData.item}</Text>
+            </View>
+          )
+        }} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ App.styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    mainContainer: {
+      padding: 32,
+    },
+  
+    inputContainer: {
+    },
+  
+    courseGoalInput: {
+      borderBottomColor: "black",
+      borderBottomWidth: 1,
+      paddingBottom: 8,
+      marginBottom: 8,
+    },
+
+    listItem: {
+      padding: 12,
+      marginVertical: 8,
+      backgroundColor: '#ccc',
+      borderColor: 'black',
+      borderWidth: 1
+    }
+  });
+
+  export default styles;
